6de45de3191dba52587e5a7b01e10f28e037dc60
golangframework <server.ozga@gmail.com>
2025-01-22 20:38:02 +0100

    Types.

--- ValuePair.go
+++ ValuePair.go
@@ -1,6 +1,10 @@
 package generic
 
-type ValuePair[T1 any, T2 any] struct {
-	Item1 T1
-	Item2 T2
+// Represents a pair of values.
+//
+// Type1 - The type of the pair's first component.
+// Type2 - The type of the pair's second component.
+type ValuePair[Type1 any, Type2 any] struct {
+	Item1 Type1
+	Item2 Type2
 }
